49b54e6e2481243bf65adb2784c610e73512b7e3
Egor <egormuzaev0@gmail.com>
2026-06-07 09:09:15 +0300

    test commit in branch future

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test commit in branch future");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
